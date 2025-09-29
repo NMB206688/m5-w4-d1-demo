6e4fe89971d0cdc73a38ac7f028ff76272803e45
Naveen Mamidi <naveenmamidi6798@gmail.com>
2025-09-29 15:49:38 -0700

    3-Updtae

--- demo3/on-click.js
+++ demo3/on-click.js
@@ -5,34 +5,43 @@ class OnClickElements extends React.Component {
     super();
   }
 
-  //add handler
+  // add handler
+  // ADDED ↓↓↓
+  handleClick(event) {
+    alert(`hey! you clicked: ${event.target.id}`);
+  }
+  // ADDED ↑↑↑
 
   render() {
     // TODO: Your task is to add onClick listeners to each of the elements within the container-div <div>
     return (
       <div className="container-div">
-        <div id="div-element" >
+        <div id="div-element" 
+             /* ADDED ↓ */ onClick={(e) => this.handleClick(e)} /* ADDED ↑ */>
           I am DIV
         </div>
 
-        <span id="span-element" >
+        <span id="span-element" 
+              /* ADDED ↓ */ onClick={(e) => this.handleClick(e)} /* ADDED ↑ */>
           I am SPAN
         </span>
         <br></br>
 
-        <button id="button-element" >
+        <button id="button-element" 
+                /* ADDED ↓ */ onClick={(e) => this.handleClick(e)} /* ADDED ↑ */>
           I am Button
         </button>
         <br></br>
 
-        <a id="link-element" href="" >
+        <a id="link-element" href=""
+           /* ADDED ↓ */ onClick={(e) => this.handleClick(e)} /* ADDED ↑ */>
           I am LINK
         </a>
 
         <div
           id="div-element-2"
           className="button"
-          
+          /* ADDED ↓ */ onClick={(e) => this.handleClick(e)} /* ADDED ↑ */
         >
           I am DIV
         </div>
@@ -40,7 +49,7 @@ class OnClickElements extends React.Component {
         <span
           id="span-element-2"
           className="button"
-          
+          /* ADDED ↓ */ onClick={(e) => this.handleClick(e)} /* ADDED ↑ */
         >
           I am SPAN
         </span>
@@ -49,7 +58,7 @@ class OnClickElements extends React.Component {
         <button
           id="button-element-2"
           className="button"
-          
+          /* ADDED ↓ */ onClick={(e) => this.handleClick(e)} /* ADDED ↑ */
         >
           I am Button
         </button>
@@ -59,7 +68,7 @@ class OnClickElements extends React.Component {
           id="link-element-2"
           className="button"
           href=""
-          
+          /* ADDED ↓ */ onClick={(e) => this.handleClick(e)} /* ADDED ↑ */
         >
           I am LINK
         </a>
